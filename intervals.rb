687d0a7fce20827ef3024877606cd3e69e03cd56
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2011-03-30 18:20:27 -0300

    comecamos de novo

--- intervals.rb
+++ intervals.rb
@@ -1,15 +1,11 @@
 def generate_intervals(values)
-    values = values.sort
-    ret = []
-    val = [values.first, values.first]
-    ret << val;
-    for i in 1..values.length-1 do
-        if ((values[i] - values[i-1]) != 1 )
-            val = [values[i],values[i]]
-            ret << val;
-        else
-            val[1] = values[i]
-        end
+    
+    intervals = [[values.first,values.last]]
+    
+    if(values.last-values.first > 1)
+       intervals = [[values.first,values.first],[values.last, values.last]]
     end
-    return ret
+    
+    intervals
+    
 end
--- intervals_spec.rb
+++ intervals_spec.rb
@@ -8,15 +8,7 @@ describe 'intervals' do
     it 'should return an interval with 2 values in sequence' do 
        generate_intervals([100,101]).should == [[100, 101]]
     end
-    
-    it 'should return an interval with 3 values in sequence' do 
-       generate_intervals([100,101,102]).should == [[100, 102]]
-    end
-    
-    it 'should return an interval with 4 values in sequence unsorted' do 
-       generate_intervals([100,101,99,102]).should == [[99, 102]]
-    end
-    
+
     it 'should return two unary intervals with 1 value each' do 
        generate_intervals([100,103]).should == [[100, 100],[103, 103]]
     end
